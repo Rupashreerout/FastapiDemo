353a23b9322b8f6fee2c027b44075cd034638a6d
Rupashree Routray <rupashreeroutray4@gmail.com>
2026-01-14 13:35:50 +0530

    Initial FastAPI project

--- fastapi_project/database.py
+++ fastapi_project/database.py
@@ -1,13 +1,40 @@
+# from fastapi import Depends
+# from typing import Annotated
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base,DeclarativeMeta
+# from sqlalchemy.orm import sessionmaker,Session
+
+# DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/fastapidemo"
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# BASE = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+# db_dependency = Annotated[Session,Depends(get_db)]
+
 from fastapi import Depends
 from typing import Annotated
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base,DeclarativeMeta
-from sqlalchemy.orm import sessionmaker,Session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/fastapidemo"
+DATABASE_URL = "postgresql+psycopg2://postgres:2001@localhost:5432/fastapidemo"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+engine = create_engine(DATABASE_URL, echo=True)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
 BASE = declarative_base()
 
 def get_db():
@@ -17,4 +44,6 @@ def get_db():
     finally:
         db.close()
 
-db_dependency = Annotated[Session,Depends(get_db)]
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
